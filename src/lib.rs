aaf6c196a5fcf440bd6509bcfce2932e3876281e
Nikita Baksalyar <nikita.baksalyar@gmail.com>
2015-11-14 22:58:21 +0300

    Initial import from nbaksalyar/rust-chat

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+extern crate mio;
+extern crate http_muncher;
+extern crate sha1;
+extern crate rustc_serialize;
+extern crate byteorder;
+
+mod frame;
+mod client;
+mod http;
+mod server;
+pub mod interface;
